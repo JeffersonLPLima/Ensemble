52b97f7a8ac14d91444de182b8f52ed4028855ec
Jefferson Luiz Pessoa Lima <jeffersonlima163@gmail.com>
2018-08-29 13:53:48 -0300

    Update lista_1.py

--- lista_1.py
+++ lista_1.py
@@ -1,6 +1,6 @@
 # scikit-learn k-fold cross-validation
 from numpy import array
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold,StratifiedKFold
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
@@ -11,23 +11,30 @@ import numpy as np
 
 
 #metrics
-from sklearn.metrics import f1_score, roc_curve, auc
-from imblearn.metrics import geometric_mean_score
+from sklearn.metrics import f1_score, roc_curve, auc, roc_auc_score
+##from imblearn.metrics import geometric_mean_score
 
+dataset = 'data_1'
 
 # Generate a classification dataset
-data = np.genfromtxt("data_1.txt",delimiter=',',dtype='str')
+data = np.genfromtxt(dataset+".txt",delimiter=',',dtype='str')
 print(data.shape)
                         #column
 X = np.delete(data, data.shape[1]-1, axis=1) 
 y = data[:,data.shape[1]-1]
-y_=[]
-for i in range(len(y)):
-    if(y[i]=='false' or y[i]=='NDEF'):
-        y_.append(0)
-    else:
-        y_.append(1)        
 
+y_=[]
+_false=0
+_true=0
+for i in range(len(y)):
+    if(y[i]=='false'):
+        y_.append(0)
+        _false+=1
+    else:
+        _true+=1
+        y_.append(1)
+
+input()      
 y = np.array(y_)
 X = X.astype(np.float)
 
@@ -41,8 +48,8 @@ f1=[]
 gmean=[]
 auc=[]
 fold=0 
-kfold = KFold(n_splits=10, shuffle=False, random_state=1) 
-for train, test in kfold.split(X):
+skfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=30) 
+for train, test in skfold.split(X,y):
     Xi_train = X[train]
     yi_train = y[train]
 
@@ -55,35 +62,42 @@ for train, test in kfold.split(X):
     auc_subsample={}
 
     print("fold:",fold)
+    print(Xi_train.shape)
+    print(Xi_test.shape)
+    print(yi_train.shape)
+    print(yi_test.shape)
     fold+=1
     for i in range(5,11):
         sub_X = Xi_train[:int(Xi_train.shape[0]*i/10)]
         sub_y = yi_train[:int(Xi_train.shape[0]*i/10)]
+
         base_model = Perceptron(max_iter=100)
         #Commun Bagging (samples bootstrap)
-        pool_classifiers = BaggingClassifier(base_model, n_estimators=100)
+        #pool_classifiers = BaggingClassifier(base_model,bootstrap=True, n_estimators=100)
         #Random Subspace (features bootstrap)
-        #pool_classifiers = BaggingClassifier(base_model, n_estimators=100,Bootstrap = True, bootstrap_features = True, max_features = 0.5)
+        pool_classifiers = BaggingClassifier(base_model, n_estimators=100, bootstrap_features = True, max_features = 0.5)
         pool_classifiers.fit(sub_X, sub_y)
         
         y_pred = pool_classifiers.predict(Xi_test)
         acc_subsample[i/10]= np.mean(y_pred == yi_test)
         f1_subsample[i/10]= f1_score(yi_test, y_pred,average='weighted')
-        gmean_subsample[i/10]= geometric_mean_score(yi_test, y_pred, average='weighted')
-        fpr, tpr, thresholds = roc_curve(yi_test, y_pred, pos_label=2)
+        #gmean_subsample[i/10]= geometric_mean_score(yi_test, y_pred, average='weighted')
+        auc_subsample[i/10] = roc_auc_score(yi_test, y_pred,average='weighted')
+
+        #fpr, tpr, thresholds = roc_curve(yi_test, y_pred)
         #auc_subsample[i/10]= auc(fpr, tpr)
         
 
         
     accuracy.append(acc_subsample)
     f1.append(f1_subsample)
-    gmean.append(gmean_subsample)
-##    auc.append(auc_subsample)
+#    gmean.append(gmean_subsample)
+    auc.append(auc_subsample)
 
-np.save("Perceptron_bagging_acc.py", accuracy)
-np.save("Perceptron_bagging_f1.py", f1)
-np.save("Perceptron_bagging_gmean.py", gmean)
-##np.save("Perceptron_bagging_auc.py", auc)
+##np.save(dataset+"Perceptron_bagging_acc.py", accuracy)
+##np.save(dataset+"Perceptron_bagging_f1.py", f1)
+##np.save(dataset+"Perceptron_bagging_gmean.py", gmean)
+##np.save(dataset+"Perceptron_bagging_auc.py", auc)
         
 
 
